14a82598d98f1eb892603e734008f97ede2b4dd2
Happy2018new <chenbo79800@163.com>
2026-02-09 10:10:14 +0800

    script/server/cmd.py: Declare the type in advance within the class

--- behavior_pack/script/server/cmd.py
+++ behavior_pack/script/server/cmd.py
@@ -35,6 +35,22 @@ class CommandHandlers:
     CommandHandlers 是所有自定义命令的总处理设备
     """
 
+    command_block_output = None  # type: CommandBlockOutputHandler | None
+    compile_cache = None  # type: CompileCacheHandler | None
+    custom_form = None  # type: CustomFormHandler | None
+    custom_function = None  # type: CustomFunctionHandler | None
+    edit_button = None  # type: EditButtonHandler | None
+    edit_dropdown = None  # type: EditDropdownHandler | None
+    edit_input = None  # type: EditInputHandler | None
+    edit_label = None  # type: EditLabelHandler | None
+    edit_long_form = None  # type: EditLongFormHandler | None
+    edit_modal_form = None  # type: EditModalFormHandler | None
+    edit_popup_form = None  # type: EditPopupFormHandler | None
+    edit_slider = None  # type: EditSliderHandler | None
+    edit_step_slider = None  # type: EditStepSliderHandler | None
+    edit_toggle = None  # type: EditToggleHandler | None
+    system_event = None  # type: SystemEventHandler | None
+
     def __init__(
         self,
         compile_cache,  # type: CompileCache
@@ -89,32 +105,47 @@ class CommandHandlers:
         """
         command = args["command"]  # type: str
         if command == "commandblockoutput":
+            assert self.command_block_output is not None
             self.command_block_output.on_custom_command_trigger(args)
         elif command == "compilecache":
+            assert self.compile_cache is not None
             self.compile_cache.on_custom_command_trigger(args)
         elif command == "customform":
+            assert self.custom_form is not None
             self.custom_form.on_custom_command_trigger(args)
         elif command == "customfunction":
+            assert self.custom_function is not None
             self.custom_function.on_custom_command_trigger(args)
         elif command == "editbutton":
+            assert self.edit_button is not None
             self.edit_button.on_custom_command_trigger(args)
         elif command == "editdropdown":
+            assert self.edit_dropdown is not None
             self.edit_dropdown.on_custom_command_trigger(args)
         elif command == "editinput":
+            assert self.edit_input is not None
             self.edit_input.on_custom_command_trigger(args)
         elif command == "editlabel":
+            assert self.edit_label is not None
             self.edit_label.on_custom_command_trigger(args)
         elif command == "editlongform":
+            assert self.edit_long_form is not None
             self.edit_long_form.on_custom_command_trigger(args)
         elif command == "editmodalform":
+            assert self.edit_modal_form is not None
             self.edit_modal_form.on_custom_command_trigger(args)
         elif command == "editpopupform":
+            assert self.edit_popup_form is not None
             self.edit_popup_form.on_custom_command_trigger(args)
         elif command == "editslider":
+            assert self.edit_slider is not None
             self.edit_slider.on_custom_command_trigger(args)
         elif command == "editstepslider":
+            assert self.edit_step_slider is not None
             self.edit_step_slider.on_custom_command_trigger(args)
         elif command == "edittoggle":
+            assert self.edit_toggle is not None
             self.edit_toggle.on_custom_command_trigger(args)
         elif command == "systemevent":
+            assert self.system_event is not None
             self.system_event.on_custom_command_trigger(args)
