5954a958b3aa4b371d1b193930ab6c0a85b16af3
Happy2018new <chenbo79800@163.com>
2026-02-25 11:01:37 +0800

    script/server/executor/language/builtins/static/checker/checker.py: Improve object checker

--- behavior_pack/script/server/executor/language/builtins/static/checker/checker.py
+++ behavior_pack/script/server/executor/language/builtins/static/checker/checker.py
@@ -8,6 +8,28 @@ import types
 from .const import SAFE_MODULE, UNSAFE_BUILTINS
 
 
+def is_string(obj):  # type: (Any) -> bool
+    """
+    is_string 检查 obj 是否是字符串
+
+    Args:
+        obj (Any): 待检查的对象
+
+    Returns:
+        bool: 目标对象是否是字符串
+    """
+    if isinstance(obj, str):
+        return True
+
+    try:
+        if isinstance(obj, unicode):  # type: ignore
+            return True
+    except Exception:
+        pass
+
+    return False
+
+
 def check_object(obj):  # type: (Any) -> bool
     """
     check_object 检查 obj 所指示的对象是否是我们允许的存在。
@@ -27,12 +49,27 @@ def check_object(obj):  # type: (Any) -> bool
             return False
 
     if callable(obj):
-        try:
-            module_name = getattr(obj, "__module__")
-            if module_name in ("__builtin__", "builtins"):
-                name = getattr(obj, "__name__", "").lower()
-                return name not in UNSAFE_BUILTINS
-        except:
-            pass
+        mark = False
+
+        if not hasattr(obj, "__module__"):
+            return True
+        module_name = getattr(obj, "__module__")
+        if not is_string(module_name):
+            return False
+
+        if module_name in ("__builtin__", "site"):
+            mark = True
+        if module_name in ("_sitebuiltins", "builtins", "_io"):
+            mark = True
+        if not mark:
+            return True
+
+        if not hasattr(obj, "__name__"):
+            return False
+        func_name = getattr(obj, "__name__", "")
+        if not is_string(func_name):
+            return False
+
+        return func_name.lower() not in UNSAFE_BUILTINS
 
     return True
